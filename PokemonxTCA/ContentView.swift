c430d77bc3d0a3b5ca25d217ac05c9306cbc7a40
tsuyoshi.matsumaru <tsuyoshi.matsumaru@uzabase.com>
2024-05-08 10:39:21 +0900

    Initial Commit

---
+++ PokemonxTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonxTCA
+//
+//  Created by tsuyoshi.matsumaru on 2024/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonxTCA/PokemonxTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonxTCAApp.swift
+//  PokemonxTCA
+//
+//  Created by tsuyoshi.matsumaru on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct PokemonxTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
